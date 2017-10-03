f9e7b01989bd32669bf48b87939f5d758d78bb0f
Mario Pabon <me@mariopabon.com>
2017-10-03 12:43:23 -0400

    Update example test

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 const React = require('react');
+const expect = require('expect');
+const {spy} = require('sinon');
 
 const reactUtils = require('./');
 
-function TestComponent () {
+function TestComponent ({onClick}) {
   return (
-    <div className="test-component">Hi!</div>
+    <button className="test-button" onClick={onClick}>Hi!</button>
   );
 }
 
@@ -12,7 +14,11 @@ describe('An example test', function () {
   const {render} = reactUtils();
 
   it('passes', function () {
-    const wrapper = render(<TestComponent />);
-    expect(wrapper.text()).toContain('Hi!');
+    const onClick = spy();
+    const wrapper = render(<TestComponent onClick={onClick} />);
+    const button = wrapper.find('.test-button');
+    expect(button.text()).toContain('Hi!');
+    button.simulate('click');
+    expect(onClick.callCount).toEqual(1);
   });
 });
